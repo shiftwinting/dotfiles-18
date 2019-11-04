53029916f0fc255bf802ef7f6eb948b9bd8479b0
Thomas Vigouroux <tomvig38@gmail.com>
2019-11-04 18:51:45 +0100

    config: add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,50 @@
+set -x EDITOR nvim
+set -x FZF_DEFAULT_OPTS "--preview=\"bat --color=always --style numbers,grid,changes {}\""
+set -x FZF_DEFAULT_COMMAND "fd --color=never"
+set -x MANPAGER "nvim -c 'set ft=man' -c 'set laststatus=0' -c 'set noruler' -"
+
+set -x PATH $PATH $HOME/.luarocks/bin
+
+function nvimfzf
+    nvim +"Files! $argv[1]"
+end
+
+abbr vi nvim
+abbr py python3.7
+abbr nf nvimfzf
+
+alias pip pip3.7
+alias python python3.7
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    # fish_default_key_bindings -M insert
+    # Without an argument, fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings insert
+end
+
+function fish_mode_prompt
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+                echo ']1337;CursorShape=0'
+            case insert
+                echo ']1337;CursorShape=1'
+            case replace_one
+                echo ']1337;CursorShape=2'
+            case visual
+                echo ']1337;CursorShape=2'
+        end
+    end
+end
+
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
+
+# source ~/.iterm2_shell_integration.(basename $SHELL)
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/Thomas/.local/share/omf/themes/default/fish_prompt.fish
