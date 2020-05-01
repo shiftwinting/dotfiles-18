fe178fc74f8fe97aceae916077e9444e37d25e79
Thomas Vigouroux <tomvig38@gmail.com>
2020-05-01 09:21:34 +0200

    update fish config

--- base/.config/fish/config.fish
+++ base/.config/fish/config.fish
@@ -1,12 +1,15 @@
-set -x EDITOR nvim
+set -x EDITOR /usr/local/bin/nvim
 set -x FZF_DEFAULT_OPTS "--preview=\"bat --color=always --style numbers,grid,changes {}\""
 set -x FZF_DEFAULT_COMMAND "fd --color=never"
-set -x MANPAGER "nvim +\"set ft=man\" -" # | set laststatus=0 | set noruler
 
-set -x PATH $PATH $HOME/.luarocks/bin /opt/texlive/2019/bin/x86_64-linux
+set -x PATH $PATH $HOME/.luarocks/bin $HOME/scripts $HOME/.local/bin
 
 function nvimfzf
     nvim +"Files! $argv[1]"
+end
+
+function man
+	nvim +"Man $argv[1]" +only 
 end
 
 function tig
@@ -16,6 +19,7 @@ end
 abbr vi nvim
 abbr py python3.7
 abbr nf nvimfzf
+abbr zf 'zathura (fd ".*.pdf" | fzf)'
 
 alias pip pip3.7
 alias python python3.7
@@ -34,14 +38,24 @@ function fish_mode_prompt
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
         switch $fish_bind_mode
-            case default
-                echo ']1337;CursorShape=0'
+			case default
+                echo '[2 q' # block
             case insert
-                echo ']1337;CursorShape=1'
+                echo '[6 q' # pipe
             case replace_one
-                echo ']1337;CursorShape=2'
+                echo '[4 q' # underline
+            case replace
+                echo '[4 q' # underline
             case visual
-                echo ']1337;CursorShape=2'
+                echo '[2 q' # block
+#            case default
+#                echo '[2q' # block
+#            case insert
+#                echo '[6q' # pipe
+#            case replace_one
+#                echo '[4q' # underline
+#            case visual
+#                echo '[2q' # blokc
         end
     end
 end
@@ -51,8 +65,8 @@ set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
 
-# set -x LPASS_ASKPASS ssh_askpass
-# set -x LPASS_PINENTRY ssh_askpass
+set -x LPASS_ASKPASS /home/thomas/scripts/askpass.sh
+set -x LPASS_PINENTRY /home/thomas/scripts/askpass.sh
 
 # source ~/.iterm2_shell_integration.(basename $SHELL)
 source ~/.cargo/env
